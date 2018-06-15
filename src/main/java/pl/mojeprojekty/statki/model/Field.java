d95a709de502caf07f4feac1006e7adf270f5a0e
Bartek Polak <bpolak28@gmail.com>
2018-06-15 12:21:33 +0200

    [update] - zmiana nazwy pakietu

---
+++ src/main/java/pl/mojeprojekty/statki/model/Field.java
@@ -0,0 +1,42 @@
+package pl.mojeprojekty.statki.model;
+
+public class Field {
+
+    private String type;
+
+    private String location;
+
+    private boolean hit = false;
+
+    public Field(String type) {
+        this.type = type;
+    }
+
+    public Field(){
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isHit() {
+        return hit;
+    }
+
+    public void setHit(boolean hit) {
+        this.hit = hit;
+    }
+
+    @Override
+    public String toString() {
+        return "Field{" +
+                "type='" + type + '\'' +
+                ", hit=" + hit +
+                '}';
+    }
+}
